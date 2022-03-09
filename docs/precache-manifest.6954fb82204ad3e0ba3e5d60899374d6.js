885103db6e23498e2806e4d77da0e7cceb2152f4
Rob Bracegirdle <rob269@hotmail.co.uk>
2022-03-09 19:07:03 +0000

    I just committed to master, I am a bad programmer

---
+++ docs/precache-manifest.6954fb82204ad3e0ba3e5d60899374d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f937319bed1925ea4300ea5cdfc7b78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21a91a9ce07da9d36150",
+    "url": "/static/css/main.b8293437.chunk.css"
+  },
+  {
+    "revision": "632bf5600a3427442972",
+    "url": "/static/js/2.7db020fd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7db020fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a91a9ce07da9d36150",
+    "url": "/static/js/main.1aff00c9.chunk.js"
+  },
+  {
+    "revision": "6ce07bb538fd07ff8ae5",
+    "url": "/static/js/runtime-main.6c8890a7.js"
+  },
+  {
+    "revision": "3ada9815c06d2619d52d16a5601c96b2",
+    "url": "/static/media/Early-GameBoy.3ada9815.ttf"
+  }
+]);
